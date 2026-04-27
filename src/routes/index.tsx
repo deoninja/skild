99b3da3d2fe26e1e1b7248e2202d65cf31d75f90
Deo <deotri4@gmail.com>
2026-04-27 23:24:16 +0800

    refactor: simplify Home component and update CSS variables for improved theming

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,8 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-   <main className='page-wrap px-4 pb-8 pt-14'>
+   <main >
     <h1>Welcome to TanStack Start</h1>
-    <p>Edit <code>src/routes/index.tsx</code> to get started.</p>
    </main>
   )
 }
