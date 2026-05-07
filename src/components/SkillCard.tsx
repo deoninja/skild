2045459b4ac2d5e409b81806a4d691902000ff76
Deo <deotri4@gmail.com>
2026-05-07 21:48:16 +0800

    refactor

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -28,6 +28,7 @@ const SkillCard = ({
 			// Reset the "copied" state after 2 seconds
 			setTimeout(() => setIsCopied(false), 2000);
 		} catch (err) {
+			setIsCopied(false);
 			console.error("Failed to copy command:", err);
 		}
 	};
@@ -56,7 +57,11 @@ const SkillCard = ({
 						<img src="/logo512.png" alt="author avatar" className="avatar" />
 						<div className="author-copy">
 							<p>Deo</p>
-							<p>{new Date(createdAt as string).toLocaleDateString()}</p>
+							<p>
+								{createdAt
+									? new Date(createdAt as string).toLocaleDateString()
+									: "Unknown date"}
+							</p>
 						</div>
 					</div>
 					<p className="category">{category}</p>
